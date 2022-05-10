1d03958a24b2c14f7d862d35ea14519f01e47825
Eungwang <eungwang1203@gmail.com>
2022-05-10 22:21:30 +0900

    fetch : HomeCategory 레이아웃개발

---
+++ src/components/Home/HomeCategoryView.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { starEmoji } from '@assets/emoji';
+import Button from '@components/Common/Button';
+import { Category, IPost } from '@typings/customTypes';
+import styled from 'styled-components';
+
+interface IHomeCategoryViewProps {
+  list: {
+    categoryPk: number;
+    categoryCode: string;
+    categoryName: string;
+  }[];
+  each: (data: Category) => {
+    onFilter: () => void;
+    posts: IPost[];
+    currentCategory: number;
+    categoryPk: number;
+    categoryCode: string;
+    categoryName: string;
+  };
+}
+
+const HomeCategoryView = ({ list, each }: IHomeCategoryViewProps) => {
+  return (
+    <Layout>
+      {list?.map((category) => {
+        const { onFilter, currentCategory } = each(category);
+        return (
+          <Button
+            key={category.categoryPk}
+            radius="20px"
+            variant={currentCategory === category.categoryPk ? 'primary' : 'default'}
+            size="md"
+            onClick={onFilter}
+          >
+            {category.categoryName === '인기글' && <StyledImg src={starEmoji} alt="" />}
+            <StyledSpan>{category.categoryName}</StyledSpan>
+          </Button>
+        );
+      })}
+    </Layout>
+  );
+};
+
+export default HomeCategoryView;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-width: 100vw;
+  overflow-x: scroll;
+  padding: 20px 0;
+  -ms-overflow-style: none; /* IE, Edge */
+  scrollbar-width: none; /* Firefox */
+  ::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera */
+  }
+`;
+
+const StyledImg = styled.img`
+  width: 1.375rem;
+  height: 1.375rem;
+`;
+
+const StyledSpan = styled.span`
+  + img {
+    margin-left: 4px;
+  }
+`;
