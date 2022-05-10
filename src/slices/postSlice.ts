96bdaa200aa810badc020b0c3cc11f3acf22f2bf
Eungwang <eungwang1203@gmail.com>
2022-05-10 22:24:54 +0900

    fetch : category필터 reducer개발 + writtenAt 형태 변환

--- src/slices/postSlice.ts
+++ src/slices/postSlice.ts
@@ -1,9 +1,12 @@
 import { getPosts } from '@actions/post';
-import { createSlice } from '@reduxjs/toolkit';
-import { IPostState } from '@typings/customTypes';
+import { generateBetweenTime } from '@lib/generateBetweenTime';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IPost, IPostState } from '@typings/customTypes';
 
 export const initialState: IPostState = {
   posts: [],
+  filteredPosts: null,
+  filteredCategoryPk: 0,
   post: null,
   getPostsLoading: false,
   getPostsDone: false,
@@ -13,20 +16,48 @@ export const initialState: IPostState = {
 export const postSlice = createSlice({
   name: 'post',
   initialState,
-  reducers: {},
+  reducers: {
+    onCategoryFilter: (state, action) => {
+      if (state.posts) {
+        // action.payload === 7 : 인기글
+        if (action.payload === 7) {
+          state.filteredPosts = state.posts.filter((el) => el.viewCount >= 100);
+          state.filteredCategoryPk = action.payload;
+          return;
+        }
+        // // action.payload === 6 : 전체글
+        if (action.payload === 6) {
+          state.filteredPosts = null;
+          state.filteredCategoryPk = action.payload;
+          return;
+        }
+        state.filteredPosts = state.posts.filter((el) => el.categoryPk === action.payload);
+        state.filteredCategoryPk = action.payload;
+      }
+    },
+    onCategoryReset: (state) => {
+      state.filteredPosts = null;
+      state.filteredCategoryPk = 0;
+    },
+  },
   extraReducers: (builder) =>
     builder
       .addCase(getPosts.pending, (state) => {
         state.getPostsLoading = true;
       })
-      .addCase(getPosts.fulfilled, (state, action) => {
+      .addCase(getPosts.fulfilled, (state, action: PayloadAction<IPost[]>) => {
         state.getPostsDone = true;
         state.getPostsLoading = false;
-        state.posts = action.payload;
+        const posts = action.payload.map((post) => {
+          const writtenAt = generateBetweenTime(post);
+          return { ...post, writtenAt };
+        });
+        state.posts = posts;
       })
       .addCase(getPosts.rejected, (state, action: ReturnType<typeof getPosts.rejected>) => {
         state.getPostsLoading = false;
         state.getPostsError = action.error;
       }),
 });
+export const { onCategoryFilter, onCategoryReset } = postSlice.actions;
 export default postSlice.reducer;
