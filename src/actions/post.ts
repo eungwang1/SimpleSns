fb4ff59acb1d29c45bd6aae0f37cad4660445255
Eungwang <eungwang1203@gmail.com>
2022-05-11 14:03:32 +0900

    fetch : 좋아요, 조회수 reducer,action추가

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -1,9 +1,64 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { IPostState, likedState } from '@typings/customTypes';
 import axios from 'axios';
 
-export const getPosts = createAsyncThunk('post/getRooms', async () => {
+export const getPosts = createAsyncThunk('post/getPosts', async () => {
   try {
     const res = await axios.get('http://localhost:4000/posts');
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+export const getPost = createAsyncThunk('post/getPost', async (pk: string) => {
+  try {
+    const res = await axios.get('http://localhost:4000/posts', {
+      params: {
+        pk,
+      },
+    });
+    return res.data[0];
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+type dataForheart = {
+  post_pk: string;
+  likedState: likedState;
+};
+
+export const heartPost = createAsyncThunk('post/heartPost', async (data: dataForheart, thunkAPI) => {
+  const { postSlice } = thunkAPI.getState() as { postSlice: IPostState };
+  const currentLikeCount = postSlice.post ? postSlice.post?.likeCount : 0;
+  let likeCount = 0;
+  let likedState: likedState = 'unliked';
+  if (data.likedState === 'unliked') {
+    likeCount = currentLikeCount + 1;
+    likedState = 'liked';
+  } else if (data.likedState === 'liked') {
+    likeCount = currentLikeCount - 1;
+    likedState = 'unliked';
+  }
+  try {
+    const res = await axios.patch(`http://localhost:4000/posts/${data.post_pk}`, {
+      likeCount,
+      likedState,
+    });
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+export const viewPost = createAsyncThunk('post/viewPost', async (post_pk: string, thunkAPI) => {
+  const { postSlice } = thunkAPI.getState() as { postSlice: IPostState };
+  const currentViewCount = postSlice.post ? postSlice.post?.viewCount : 0;
+  try {
+    const res = await axios.patch(`http://localhost:4000/posts/${post_pk}`, {
+      viewCount: currentViewCount + 1,
+    });
     return res.data;
   } catch (e) {
     console.log(e);
--- src/slices/postSlice.ts
+++ src/slices/postSlice.ts
@@ -1,4 +1,4 @@
-import { getPosts } from '@actions/post';
+import { getPost, getPosts, heartPost, viewPost } from '@actions/post';
 import { generateBetweenTime } from '@lib/generateBetweenTime';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IPost, IPostState } from '@typings/customTypes';
@@ -11,6 +11,15 @@ export const initialState: IPostState = {
   getPostsLoading: false,
   getPostsDone: false,
   getPostsError: null,
+  getPostLoading: false,
+  getPostDone: false,
+  getPostError: null,
+  heartPostLoading: false,
+  heartPostDone: false,
+  heartPostError: null,
+  viewPostLoading: false,
+  viewPostDone: false,
+  viewPostError: null,
 };
 
 export const postSlice = createSlice({
@@ -35,10 +44,6 @@ export const postSlice = createSlice({
         state.filteredCategoryPk = action.payload;
       }
     },
-    onCategoryReset: (state) => {
-      state.filteredPosts = null;
-      state.filteredCategoryPk = 0;
-    },
   },
   extraReducers: (builder) =>
     builder
@@ -57,7 +62,49 @@ export const postSlice = createSlice({
       .addCase(getPosts.rejected, (state, action: ReturnType<typeof getPosts.rejected>) => {
         state.getPostsLoading = false;
         state.getPostsError = action.error;
+      })
+      .addCase(getPost.pending, (state) => {
+        state.getPostLoading = true;
+      })
+      .addCase(getPost.fulfilled, (state, action: PayloadAction<IPost>) => {
+        state.getPostDone = true;
+        state.getPostLoading = false;
+        const post = { ...action.payload, writtenAt: generateBetweenTime(action.payload) };
+        state.post = post;
+      })
+      .addCase(getPost.rejected, (state, action: ReturnType<typeof getPost.rejected>) => {
+        state.getPostLoading = false;
+        state.getPostError = action.error;
+      })
+      .addCase(heartPost.pending, (state) => {
+        state.heartPostLoading = true;
+      })
+      .addCase(heartPost.fulfilled, (state, action: PayloadAction<IPost>) => {
+        state.heartPostDone = true;
+        state.heartPostLoading = false;
+        if (state.post) {
+          state.post.likeCount = action.payload.likeCount;
+          state.post.likedState = action.payload.likedState;
+        }
+      })
+      .addCase(heartPost.rejected, (state, action: ReturnType<typeof heartPost.rejected>) => {
+        state.heartPostLoading = false;
+        state.heartPostError = action.error;
+      })
+      .addCase(viewPost.pending, (state) => {
+        state.viewPostLoading = true;
+      })
+      .addCase(viewPost.fulfilled, (state, action: PayloadAction<IPost>) => {
+        state.viewPostDone = true;
+        state.viewPostLoading = false;
+        if (state.post) {
+          state.post.viewCount = action.payload.viewCount;
+        }
+      })
+      .addCase(viewPost.rejected, (state, action: ReturnType<typeof viewPost.rejected>) => {
+        state.viewPostLoading = false;
+        state.viewPostError = action.error;
       }),
 });
-export const { onCategoryFilter, onCategoryReset } = postSlice.actions;
+export const { onCategoryFilter } = postSlice.actions;
 export default postSlice.reducer;
