231711a16fa6ac6b2902b9619d6e9bae2e6ffd3d
Eungwang <eungwang1203@gmail.com>
2022-05-12 14:16:53 +0900

    styled : fixed와 transform관련 이슈로 인한 글쓰기 버튼 위치 변경

--- src/components/Home/HomeContainerView.tsx
+++ src/components/Home/HomeContainerView.tsx
@@ -1,20 +1,35 @@
+import { writeIcon } from '@assets/icon';
+import Button from '@components/Common/Button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import HomeCategory from './HomeCategory';
 import HomeList from './HomeList';
-const HomeContainerView = () => {
+import { motion, Variants } from 'framer-motion';
+interface IHomeContainerView {
+  pageVariants: Variants;
+}
+const HomeContainerView = ({ pageVariants }: IHomeContainerView) => {
   return (
-    <Layout>
-      <Title>커뮤니티</Title>
-      <HomeCategory />
-      <HomeList />
-    </Layout>
+    <>
+      <Layout key="Home" variants={pageVariants} initial="initial" animate="visible" exit="leaving">
+        <Title>커뮤니티</Title>
+        <HomeCategory />
+        <HomeList />
+      </Layout>
+      <Link to="/community/post/new">
+        <Button variant="primary" size="lg" radius="8px" position="fixed" style={{ bottom: '15px', right: '15px' }}>
+          글쓰기
+          <img src={writeIcon} alt="" style={{ marginLeft: '5px' }} />
+        </Button>
+      </Link>
+    </>
   );
 };
 
 export default HomeContainerView;
 
-const Layout = styled.div`
+const Layout = styled(motion.div)`
   display: flex;
   flex-direction: column;
   padding: 34px 0px;
--- src/components/Home/HomeListView.tsx
+++ src/components/Home/HomeListView.tsx
@@ -4,7 +4,7 @@ import { IPost } from '@typings/customTypes';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import HomeContentCardView from './HomeContentCardView';
+import ContentCard from '../Common/ContentCard';
 
 interface IHomeListViewProps {
   posts: IPost[];
@@ -14,42 +14,34 @@ const HomeListView = ({ posts }: IHomeListViewProps) => {
   return (
     <Layout>
       {posts.map((post, idx) => (
-        <>
-          <StyledLink to={`/community/post/${post.pk}`}>
-            <HomeContentCardView
-              post={post}
-              idx={idx}
-              content={`${post.content.slice(0, 40)} + 
+        <StyledLink key={post.pk} to={`/community/post/${post.pk}`}>
+          <ContentCard
+            post={post}
+            idx={idx}
+            content={`${post.content.slice(0, 40)}
           ${post.content.length >= 40 ? '...' : ''}`}
-            />
-            {post.imageUrl && (
-              <ImageList>
-                <ContentImg src={post.imageUrl[0]} alt="" height="160px" width="85%" />
-              </ImageList>
-            )}
-            <ContentBottom>
-              <div className="flex-row">
-                <img src={eyeIcon} alt="" width="16px" height="16px" />
-                <span>{post.viewCount}</span>
-              </div>
-              <div className="flex-row">
-                <img src={thumbIcon} alt="" width="16px" height="16px" />
-                <span>{post.likeCount}</span>
-              </div>
-              <div className="flex-row">
-                <img src={commentIcon} alt="" width="16px" height="16px" />
-                <span>{post.commentCount}</span>
-              </div>
-            </ContentBottom>
-          </StyledLink>
-        </>
+          />
+          {post.imageUrl && post.imageUrl.length >= 1 && (
+            <ImageList>
+              <ContentImg src={post.imageUrl[0]} alt="" height="160px" width="85%" />
+            </ImageList>
+          )}
+          <ContentBottom>
+            <div className="flex-row">
+              <img src={eyeIcon} alt="" width="16px" height="16px" />
+              <span>{post.viewCount}</span>
+            </div>
+            <div className="flex-row">
+              <img src={thumbIcon} alt="" width="16px" height="16px" />
+              <span>{post.likeCount}</span>
+            </div>
+            <div className="flex-row">
+              <img src={commentIcon} alt="" width="16px" height="16px" />
+              <span>{post.commentCount}</span>
+            </div>
+          </ContentBottom>
+        </StyledLink>
       ))}
-      <Link to="/community/post/new">
-        <Button variant="primary" size="lg" radius="8px" position="fixed" style={{ bottom: '15px', right: '15px' }}>
-          글쓰기
-          <img src={writeIcon} alt="" style={{ marginLeft: '5px' }} />
-        </Button>
-      </Link>
     </Layout>
   );
 };
@@ -60,55 +52,6 @@ const Layout = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
-`;
-
-const ContentHeader = styled.div`
-  padding: 0 30px;
-  margin-top: 15px;
-  display: flex;
-  flex-direction: row;
-  .flex-col {
-    display: flex;
-    flex-direction: column;
-    margin: 0.5rem;
-  }
-  align-items: center;
-`;
-
-const Nickname = styled.div`
-  font-size: 0.75rem;
-  font-weight: 700;
-`;
-
-const CategoryAndDate = styled.div`
-  color: ${(props) => props.theme.palette.Gray03};
-  font-size: 0.75rem;
-  margin-top: 3px;
-`;
-
-const ProfileImg = styled.img<{ idx: number }>`
-  padding: 4px;
-  border-radius: 50px;
-  background-color: ${(props) => props.theme.palette.backColorList[props.idx]};
-`;
-
-const ContentBody = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Title = styled.h1`
-  padding: 0 30px;
-  margin-top: 15px;
-  font-size: 1rem;
-  font-weight: 700;
-`;
-
-const Content = styled.p`
-  padding: 0 30px;
-  margin-top: 8px;
-  font-size: 0.875rem;
-  color: ${(props) => props.theme.palette.Gray04};
 `;
 
 const ImageList = styled.div`
