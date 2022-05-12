a6b8e7b40411e0d7b024bc457249d2a51051540a
Eungwang <eungwang1203@gmail.com>
2022-05-12 14:12:37 +0900

    fetch : useScroll훅 개발

---
+++ src/lib/useScroll.ts
@@ -0,0 +1,30 @@
+import { useState, useCallback } from 'react';
+
+const useScroll = (page: string) => {
+  const [scrollY, setScrollY] = useState('');
+  const scrollSave = useCallback(() => {
+    const scrollPos = window.scrollY + '';
+    setScrollY(scrollPos);
+    console.log(scrollPos);
+    return localStorage.setItem(`${page}_scroll_pos`, scrollPos);
+  }, []);
+
+  const scrollRemove = useCallback(() => {
+    setScrollY('0');
+    localStorage.removeItem(`${page}_scroll_pos`);
+  }, []);
+
+  const scrollMove = useCallback(() => {
+    const scrollPos = localStorage.getItem(`${page}_scroll_pos`);
+    localStorage.removeItem(`${page}_scroll_pos`);
+    window.scrollTo(0, Number(scrollPos));
+  }, []);
+
+  const scrollToTop = useCallback(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return { scrollSave, scrollRemove, scrollMove, scrollToTop };
+};
+
+export default useScroll;
