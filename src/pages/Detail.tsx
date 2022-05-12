e6c6e8284897361447166a5e024dd8692d024187
Eungwang <eungwang1203@gmail.com>
2022-05-12 14:14:37 +0900

    chore : 경로이동 및 네이밍 변경

--- src/pages/Detail.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-const Detail = () => {
-  const params = useParams();
-  const { post_pk } = params;
-  return <div>{post_pk}</div>;
-};
-
-export default Detail;
