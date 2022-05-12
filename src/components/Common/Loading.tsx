7ab94f8564a4dbc96821557469421a3795516e62
Eungwang <eungwang1203@gmail.com>
2022-05-13 01:20:37 +0900

    styled : Loading Component Css수정

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TailSpin } from 'react-loader-spinner';
 const Loading = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh', justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+    <div style={{ height: '100vh', justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
       <TailSpin color="#00BFFF" height={80} width={80} />
     </div>
   );
