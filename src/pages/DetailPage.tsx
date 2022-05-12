4e043b7c765aa1138613136d5ecfeb0aaa017ba1
Eungwang <eungwang1203@gmail.com>
2022-05-12 14:20:08 +0900

    chore : 네이밍변경

---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,20 @@
+import ContentDetail from '@components/Detail/ContentDetail';
+import useScroll from '@lib/useScroll';
+import React, { useLayoutEffect } from 'react';
+import { motion } from 'framer-motion';
+import { useMotionVariants } from '@lib/useMotionVariants';
+
+const DetailPage = () => {
+  const { scrollToTop } = useScroll('DetailPage');
+  const { pageVariants } = useMotionVariants('left');
+  useLayoutEffect(() => {
+    scrollToTop();
+  }, []);
+  return (
+    <motion.div key="Home" variants={pageVariants} initial="initial" animate="visible" exit="leaving">
+      <ContentDetail />
+    </motion.div>
+  );
+};
+
+export default DetailPage;
---
+++ src/pages/PostPage.tsx
@@ -0,0 +1,12 @@
+import Post from '@components/Post/Post';
+import React from 'react';
+
+const PostPage = () => {
+  return (
+    <div>
+      <Post />
+    </div>
+  );
+};
+
+export default PostPage;
