e0a18769ec69e775ee351a3e8404f7bcd1055deb
Eungwang <eungwang1203@gmail.com>
2022-05-12 16:28:18 +0900

    fetch : 스크롤유지설정

--- src/components/Home/HomeList.tsx
+++ src/components/Home/HomeList.tsx
@@ -1,11 +1,15 @@
+import useScroll from '@lib/useScroll';
 import { useAppSelector } from '@store/hook';
-import { IPost } from '@typings/customTypes';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { VACList } from 'react-vac';
 import HomeListView from './HomeListView';
-
 const HomeList = () => {
   const { posts, filteredPosts } = useAppSelector((state) => state.postSlice);
+  const { scrollSave, scrollMove } = useScroll('HomeList');
+  useLayoutEffect(() => {
+    scrollMove();
+    return () => scrollSave();
+  }, []);
   const HomeCategoryProps = {
     posts: filteredPosts ?? posts,
   };
